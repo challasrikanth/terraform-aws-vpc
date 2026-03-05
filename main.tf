8a8b25c1a5b701210c5fe860e509032e819fc0fd
srikanth <csri@nuuday.com>
2026-03-05 16:24:12 +0530

    updated values

--- main.tf
+++ main.tf
@@ -156,14 +156,14 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table_association" "private" {
     count = length(var.private_subnet_cidrs)
-    subnet_id = var.private_subnet_tags[count.index].id
+    subnet_id = aws_subnet.private[count.index].id
     route_table_id = aws_route_table.private.id
   
 }
 
 resource "aws_route_table_association" "database" {
     count = length(var.database_subnet_cidrs)
-    subnet_id = var.database_subnet_cidrs[count.index].id
+    subnet_id = aws_subnet.database[count.index].id
     route_table_id = aws_route_table.database.id
   
 }
